50e658fea795b9d8548968508a50a656fb1cac98
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-10-04 19:57:14 -0500

    Add date and time display functionality to tasks
    Updated the PostTaskRecord and TaskRecord classes to now include methods that convert their timestamp fields into a formatted date and time string. This is done to allow a more human-readable display of these time-related properties.
    
    Also, the templates taskFragment.html and tasksFragment.html have been tweaked to leverage this new functionality and display created and updated dates for each task.
    
    Lastly, adjusted height of task display area in tasksFragment.html for better layout behavior.

--- src/main/java/dev/nathanlively/todohtmx/tasks/TaskRecord.java
+++ src/main/java/dev/nathanlively/todohtmx/tasks/TaskRecord.java
@@ -1,4 +1,53 @@
 package dev.nathanlively.todohtmx.tasks;
 
-public record TaskRecord(String createdAt, String name, String description, int id, boolean completed, int userId, String updatedAt) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskRecord {
+
+    @JsonProperty("createdAt")
+    long createdAt;
+
+    @JsonProperty("name")
+    String name;
+
+    @JsonProperty("description")
+    String description;
+
+    @JsonProperty("id")
+    int id;
+
+    @JsonProperty("completed")
+    boolean completed;
+
+    @JsonProperty("userId")
+    int userId;
+
+    @JsonProperty("updatedAt")
+    long updatedAt;
+
+    public String getUpdatedAtAsDateTime() {
+        return convertLongToFormattedDate(updatedAt);
+    }
+
+    public String getCreatedAtAsDateTime() {
+        return convertLongToFormattedDate(createdAt);
+    }
+
+    private String convertLongToFormattedDate(long dateInMillis) {
+        Instant instant = Instant.ofEpochMilli(dateInMillis);
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd-MMM-yyyy");
+        return dateTime.format(formatter);
+    }
 }
